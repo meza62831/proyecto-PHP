0158ea584a07a2521f338a62752f04eccc97396e
Michael Meza <michalexis099@gmail.com>
2022-12-28 18:22:49 -0600

    Primeros pasos a aprender.

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
 
-echo "Holis, esto está hecho con PHP \n";
-    
+// echo "Holis, esto está hecho con PHP \n";
+
+$nombre = "Carlos";
+$apellido = "Santana";
+
+/* 
+echo "No no. Yo me llamo " . $nombre . " " . $apellido . "\n";  // Mismo resultado
+
+echo "No no. Yo me llamo $nombre $apellido \n";                 // Mismo resultado pues PHP puede leer variables dentro de comillas dobles.
+ */
+
+echo "El resutlado de 4 * 5 es " . (4 * 5) . "\n";
