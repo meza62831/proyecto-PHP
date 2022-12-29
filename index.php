ab275c319fc67287c8a95e534ee83924db9f370e
Michael Meza <michalexis099@gmail.com>
2022-12-28 20:30:35 -0600

    Estudio de variables y casting

--- index.php
+++ index.php
@@ -11,4 +11,14 @@ echo "No no. Yo me llamo " . $nombre . " " . $apellido . "\n";  // Mismo resulta
 echo "No no. Yo me llamo $nombre $apellido \n";                 // Mismo resultado pues PHP puede leer variables dentro de comillas dobles.
  */
 
-echo "El resutlado de 4 * 5 es " . (4 * 5) . "\n";
+// echo "El resutlado de 4 * 5 es " . (4 * 5) . "\n";
+
+$personas = [
+    "Carlos" => 22,
+    "Mr.Michi" => 15,
+    "Juan" => 65
+];
+
+var_dump(personas);
+
+echo "/n";
